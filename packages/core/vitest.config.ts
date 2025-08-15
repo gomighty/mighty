b8b69e8d8969b92e5a48ef10bc0d288b64501be4
Amine Ilidrissi <38422328+aminevg@users.noreply.github.com>
2025-08-15 09:54:50 +0900

    feat: add CI

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     setupFiles: ["./tests/custom-matchers.ts"],
+    retry: process.env.CI ? 2 : undefined,
   },
   resolve: {
     alias: {
